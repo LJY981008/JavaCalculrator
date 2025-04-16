8687180ccd2c2d43127a6e81be6bdb1960d7e4d3
LJY981008 <88678179+LJY981008@users.noreply.github.com>
2025-04-16 11:54:26 +0900

    Feat(Calculrator) : 연산 메서드 작성
    Calculrator에 연산 메서드 작성,
    연산 결과 저장 스택 추가

--- src/lv2/Calculrator.java
+++ src/lv2/Calculrator.java
@@ -1,4 +1,20 @@
 package lv2;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
 public class Calculrator {
+    Stack<String> listResult = new Stack<>();
+    public String calculate(int[] values, char operator){
+        String result = switch (operator){
+            case '+' -> String.valueOf(values[0] + values[1]);
+            case '-' -> String.valueOf(values[0] - values[1]);
+            case '*' -> String.valueOf(values[0] * values[1]);
+            case '/' -> String.valueOf((double) values[0] + values[1]);
+            default -> "연산실패";
+        };
+
+        return result;
+    }
 }
