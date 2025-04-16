f271458e5ba9cd16a612cc50d5962dc050696fb8
LJY981008 <88678179+LJY981008@users.noreply.github.com>
2025-04-16 11:27:01 +0900

    Fix(ClaLv1) : "입력 처리 오류"
    숫자가 아닌 값을 입력하면 발생하는 오류 수정

--- src/lv1/CalLv1.java
+++ src/lv1/CalLv1.java
@@ -1,33 +1,43 @@
 package lv1;
 
+import java.io.IOException;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class CalLv1 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        String operator;
-        int value1, value2;
-        String result;
+        String operator = "";
+        int value1 = 0, value2 = 0;
+        String result = "";
+        String stopper = "";
         boolean runCalculator = true;
 
         do {
-            System.out.print("입력1 : ");
-            value1 = scanner.nextInt();
-            System.out.print("입력2 : ");
-            value2 = scanner.nextInt();
-            System.out.print("연산 방법 : ");
-            operator = scanner.next();
-            result = switch (operator.charAt(0)) {
-                case '+' -> String.valueOf(value1 + value2);
-                case '-' -> String.valueOf(value1 - value2);
-                case '*' -> String.valueOf(value1 * value2);
-                case '/' -> String.valueOf((double) value1 / value2);
-                default -> "잘못된 연산자입니다.";
-            };
-            if (result.equals("Infinity")) result = "나눗셈에 0을 넣을 수 없습니다";
-            System.out.println(result);
+            try {
+                System.out.print("입력1 : ");
+                value1 = scanner.nextInt();
+                System.out.print("입력2 : ");
+                value2 = scanner.nextInt();
+                System.out.print("연산 방법 : ");
+                operator = scanner.next();
+                result = switch (operator.charAt(0)) {
+                    case '+' -> String.valueOf(value1 + value2);
+                    case '-' -> String.valueOf(value1 - value2);
+                    case '*' -> String.valueOf(value1 * value2);
+                    case '/' -> String.valueOf((double) value1 / value2);
+                    default -> "잘못된 연산자입니다.";
+                };
+                if (result.equals("Infinity")) result = "나눗셈에 0을 넣을 수 없습니다";
+                System.out.println(result);
+            } catch (InputMismatchException e) {
+                System.out.println("잘못된 입력입니다.");
+                System.out.println("프로그램을 종료합니다.");
+                break;
+            }
             System.out.println("더 진행하시겠습니까?(exit 입력 시 종료)");
-            if (scanner.nextLine().equalsIgnoreCase("exit")) runCalculator = false;
+            stopper = scanner.next();
+            if (stopper.equalsIgnoreCase("exit")) runCalculator = false;
         } while (runCalculator);
 
     }
