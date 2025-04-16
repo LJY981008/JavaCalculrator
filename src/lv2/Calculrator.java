d81ce06e90e5431bb5c146cad7d0c4a8ab5d33e0
LJY981008 <88678179+LJY981008@users.noreply.github.com>
2025-04-16 12:01:14 +0900

    Feat(Calculrator) : setter getter 작성
    결과 저장하는 리스트 스택으로 변경,
    연산 결과 스택에 push 추가,
    setter, getter 추가

--- src/lv2/Calculrator.java
+++ src/lv2/Calculrator.java
@@ -5,7 +5,7 @@ import java.util.List;
 import java.util.Stack;
 
 public class Calculrator {
-    Stack<String> listResult = new Stack<>();
+    private Stack<String> stackResult = new Stack<>();
     public String calculate(int[] values, char operator){
         String result = switch (operator){
             case '+' -> String.valueOf(values[0] + values[1]);
@@ -14,7 +14,21 @@ public class Calculrator {
             case '/' -> String.valueOf((double) values[0] + values[1]);
             default -> "연산실패";
         };
-
+        setStackResult(result);
         return result;
+    }
+
+    public String getStackResult() {
+        if(stackResult.isEmpty()) return "연산기록이 없습니다.";
+        return stackResult.pop();
+    }
+
+    public void setStackResult(String result){
+        try {
+            Integer.parseInt(result);
+            this.stackResult.push(result);
+        }catch (NumberFormatException e){
+            System.out.println("결과가 정수가 아닙니다.");
+        }
     }
 }
