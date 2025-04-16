4dbcacabfea13cffcff0c37d36fe5c06a0ace767
LJY981008 <88678179+LJY981008@users.noreply.github.com>
2025-04-16 13:04:39 +0900

    Feat : CalLv3, Calculrator 작성
    입력 기능 추가, 연산 기능 추가, 연산자enum 추가

--- src/lv3/CalLv3.java
+++ src/lv3/CalLv3.java
@@ -1,4 +1,38 @@
 package lv3;
 
+import lv2.Calculrator;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class CalLv3 {
+    public static void main(String[] args) {
+        lv2.Calculrator calculrator = new Calculrator();
+        Scanner scanner = new Scanner(System.in);
+        int[] values = new int[2];
+        String operator = "";
+        String result = "";
+        boolean runCalculrator = true;
+
+        while (runCalculrator) {
+            String stopper = "";
+            try {
+                System.out.print("입력1 : ");
+                values[0] = scanner.nextInt();
+                System.out.print("입력2 : ");
+                values[1] = scanner.nextInt();
+                System.out.print("연산 방법 : ");
+                operator = scanner.next();
+            } catch (InputMismatchException e) {
+                System.out.println("잘못된 입력입니다.");
+                break;
+            }
+            result = calculrator.calculate(values, operator.charAt(0));
+            System.out.println("연산 결과 : " + result);
+            System.out.println("연산을 계속 진행하시겠습니까?(종료 = exit)");
+            stopper = scanner.next();
+            if(stopper.equalsIgnoreCase("exit")) runCalculrator = false;
+        }
+        System.out.println("프로그램을 종료합니다.");
+    }
 }
--- src/lv3/Calculrator.java
+++ src/lv3/Calculrator.java
@@ -1,4 +1,47 @@
 package lv3;
 
+import java.util.*;
+enum OpType{
+    PLUS('+'),MINUS('-'),MULT('*'),DIV('/');
+    final private char type;
+    public char getType(){
+        return type;
+    }
+    OpType(char type) {
+        this.type = type;
+    }
+}
+
 public class Calculrator {
+    private Queue<String> queueResult = new LinkedList<>();
+    public String calculate(int[] values, char operator){
+        String result = switch (operator){
+            case '+' -> String.valueOf(values[0] + values[1]);
+            case '-' -> String.valueOf(values[0] - values[1]);
+            case '*' -> String.valueOf(values[0] * values[1]);
+            case '/' -> String.valueOf((double) values[0] + values[1]);
+            default -> "연산실패";
+        };
+        setQueueResult(result);
+        return result;
+    }
+
+    public Queue<String> getQueueResult() {
+        if(queueResult.isEmpty()) return null;
+        return queueResult;
+    }
+
+    public void setQueueResult(String result){
+        try {
+            Integer.parseInt(result);
+            this.queueResult.offer(result);
+        }catch (NumberFormatException e){
+            System.out.println("결과가 정수가 아닙니다.");
+        }
+    }
+
+    public void removeResult(){
+        if(queueResult.isEmpty()) return;
+        queueResult.poll();
+    }
 }
