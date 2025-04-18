50581da09762502ad14dc192b44704a4bda1a410
LJY981008 <88678179+LJY981008@users.noreply.github.com>
2025-04-18 14:32:19 +0900

    Fix : 0으로 나누는 경우 수정
    0으로 나눌 수 없도록 수정
    Calculrator 캡슐화 미흡한 부분 수정
    CalLv3 불필요한 객체 생성 최소화

--- src/lv3/CalLv3.java
+++ src/lv3/CalLv3.java
@@ -8,12 +8,19 @@ import java.util.*;
 public class CalLv3 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        Calculrator<Integer> calculratorInt = new Calculrator<>();
+        Calculrator<Double> calculratorDouble = new Calculrator<>();
+
         int arraySize = 2;
+        boolean runCalculrator = true;
+        String operator;
+        String stringResult = "";
+        String inputFormat;
+
         String[] inputValues = new String[arraySize];
         Double[] doubleValues = new Double[arraySize];
         Integer[] intValues = new Integer[arraySize];
-        String operator;
-        boolean runCalculrator = true;
+
 
         while (runCalculrator) {
             // 입력
@@ -31,27 +38,23 @@ public class CalLv3 {
                 continue;
             }
 
-            // 입력 값의 정상적인지 체크하고 자료형도 체크
+            // 입력 값의 정상적인지 체크하고 자료형 체크하고 Calculrator 호출
             // String = 수가 아닐 경우, double = 실수일 경우, int = 정수일 경우
-            Calculrator<?> calculrator = null;
-            if (checkFormet(inputValues[0]).equals("String") || checkFormet(inputValues[1]).equals("String")) {
-                System.out.println("숫자를 입력해 주세요");
-            } else if (checkFormet(inputValues[0]).equals("double") || checkFormet(inputValues[1]).equals("double")) {
+            inputFormat = checkFormat(inputValues);
+            if (inputFormat.equals("double")) {
                 for (int i = 0; i < arraySize; i++) {
                     doubleValues[i] = Double.parseDouble(inputValues[i]);
                 }
-                calculrator = new Calculrator<>(doubleValues[0], doubleValues[1]);
-            } else {
+                stringResult = calculratorDouble.calculate(doubleValues[0], doubleValues[1], operator.charAt(0));
+                printResult(stringResult, calculratorDouble.getRecord());
+            } else if (inputFormat.equals("int")) {
                 for (int i = 0; i < arraySize; i++) {
                     intValues[i] = Integer.parseInt(inputValues[i]);
                 }
-                calculrator = new Calculrator<>(intValues[0], intValues[1]);
-            }
-
-            // 연산 클래스 호출
-            if (calculrator != null) {
-                String stringResult = calculrator.calculate(operator.charAt(0));
-                printResult(stringResult, Calculrator.listResult);
+                stringResult = calculratorInt.calculate(intValues[0], intValues[1], operator.charAt(0));
+                printResult(stringResult, calculratorInt.getRecord());
+            } else {
+                System.out.println("숫자를 입력해 주세요");
             }
 
             System.out.println("연산을 계속 진행하시겠습니까?(이어하기 = 아무키 입력, 종료 = exit)");
@@ -70,36 +73,41 @@ public class CalLv3 {
         System.out.println("연산 결과 : " + result);
         if (!result.equals("잘못된 연산자 입니다")) {
             // 과거 연산 기록에서 result보다 큰 값을 filter해서 저장
-            List<Double> biggers = recordResult.stream()
-                    .filter(num -> Double.parseDouble(num) > Double.parseDouble(result))
-                    .map(Double::parseDouble).toList();
+            List<Double> biggers = recordResult.stream().filter(num -> Double.parseDouble(num) > Double.parseDouble(result)).map(Double::parseDouble).toList();
             // 저장한 큰 값들 출력
+            System.out.print("결과 값보다 큰 이전 기록 : ");
             for (Double bigger : biggers) {
-                System.out.print("결과 값보다 큰 이전 기록 : ");
                 String temp = String.valueOf(bigger);
-                if (temp.endsWith(".0")) System.out.println(temp.substring(0, temp.length() - 2) + " ");
-                else System.out.println(temp);
+                if (temp.endsWith(".0")) System.out.print(temp.substring(0, temp.length() - 2) + " ");
+                else System.out.print(temp + " ");
             }
             System.out.println();
         }
     }
 
     /**
-     * 입력값이 int인지 double인지 판별하는 메서드
-     * @param value 입력값
+     * 입력값이 정수, 실수, 문자인지 판별하는 메서드
+     * @param values 입력값
      * @return 입력값의 타입
      */
-    public static String checkFormet(String value) {
-        try {
-            Integer.parseInt(value);
-            return "int";
-        } catch (NumberFormatException e) {
+    public static String checkFormat(String[] values) {
+        String format = "String";
+        for (String value : values) {
             try {
-                Double.parseDouble(value);
-                return "double";
-            } catch (NumberFormatException e2) {
-                return "String";
+                Integer.parseInt(value);
+                format = format.equals("String") ? "int" : "double";
+            } catch (NumberFormatException e) {
+                try {
+                    Double.parseDouble(value);
+                    format = "double";
+                } catch (NumberFormatException e2) {
+                    format = "String";
+                    break;
+                }
             }
+
         }
+        return format;
     }
+
 }
